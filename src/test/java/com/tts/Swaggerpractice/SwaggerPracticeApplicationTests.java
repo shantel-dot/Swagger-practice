c03262a2404b8d861be874c8fc7240671541cd16
taylorteep2 <taylorteep2@yahoo.com>
2021-03-13 11:16:41 -0600

    Initial commit

---
+++ src/test/java/com/tts/Swaggerpractice/SwaggerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.Swaggerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
